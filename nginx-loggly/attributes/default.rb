abfe97ac24ded638fca812cc5e9f25e78ece3d3a
chamikav <chamikav@salpo.com>
2017-11-17 19:55:14 +0530

    added loggly-install

---
+++ nginx-loggly/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:loggly][:auth] = "078dd418-0f5a-4437-a0da-03a6561c9063"
+default[:loggly][:account] = "chamika"
--- nginx-loggly/recipes/install-loggly.rb
+++ nginx-loggly/recipes/install-loggly.rb
@@ -7,16 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "download loggly" do
-  command "curl -O https://www.loggly.com/install/configure-nginx.sh"
-  user "root"
-  cwd "/"
+# execute "download loggly" do
+#   command "curl -O https://www.loggly.com/install/configure-nginx.sh"
+#   user "root"
+#   cwd "/"
+#   action :run
+# end
+#
+# execute "configure loggly" do
+#   command "sudo bash configure-nginx.sh -a chamika -t 078dd418-0f5a-4437-a0da-03a6561c9063 -u chamika -p Wakwella.92"
+#   user "root"
+#   cwd "/"
+#   action :run
+# end
+
+include_recipe "rsyslog"
+
+# Taken from https://www.loggly.com/docs/configure-syslog-script
+execute "install loggly" do
+  command "wget -q -O - https://www.loggly.com/install/configure-syslog.py | sudo python - setup --yes --auth 078dd418-0f5a-4437-a0da-03a6561c9063 --account chamika"
   action :run
+  not_if { node[:loggly][:auth].empty? }
 end
 
-execute "configure loggly" do
-  command "sudo bash configure-nginx.sh -a chamika -t 078dd418-0f5a-4437-a0da-03a6561c9063 -u chamika -p Wakwella.92"
-  user "root"
-  cwd "/"
-  action :run
+template "/etc/rsyslog.d/21-nginx-loggly.conf" do
+  source "loggly-nginx.conf.erb"
+  notifies :restart, "service[rsyslog]"
 end
