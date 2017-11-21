fe1076a0a4a28426be16a23564f75e3a75f83d3e
chamikav <chamikav@salpo.com>
2017-11-21 17:04:32 +0530

    rem loggly

--- nginx-loggly/recipes/install-loggly.rb
+++ nginx-loggly/recipes/install-loggly.rb
@@ -23,26 +23,26 @@
 
 include_recipe "rsyslog"
 
-apt_update "update"
+# apt_update "update"
 
-# apt_package 'python-setuptools' do
-#   action :install
+# # apt_package 'python-setuptools' do
+# #   action :install
+# # end
+# bash "Language pack install" do
+#   user "root"
+#   code <<-EOH
+#   apt-get install -y python-setuptools
+#   EOH
 # end
-bash "Language pack install" do
-  user "root"
-  code <<-EOH
-  apt-get install -y python-setuptools
-  EOH
-end
+#
+# # Taken from https://www.loggly.com/docs/configure-syslog-script
+# execute "install loggly" do
+#   command "wget -q -O - https://www.loggly.com/install/configure-syslog.py | sudo python - setup --yes --auth 078dd418-0f5a-4437-a0da-03a6561c9063 --account chamika"
+#   action :run
+#   not_if { node[:loggly][:auth].empty? }
+# end
 
-# Taken from https://www.loggly.com/docs/configure-syslog-script
-execute "install loggly" do
-  command "wget -q -O - https://www.loggly.com/install/configure-syslog.py | sudo python - setup --yes --auth 078dd418-0f5a-4437-a0da-03a6561c9063 --account chamika"
-  action :run
-  not_if { node[:loggly][:auth].empty? }
-end
-
-template "/etc/rsyslog.d/21-nginx-loggly.conf" do
-  source "loggly-nginx.conf.erb"
-  notifies :restart, "service[rsyslog]"
-end
+# template "/etc/rsyslog.d/21-nginx-loggly.conf" do
+#   source "loggly-nginx.conf.erb"
+#   notifies :restart, "service[rsyslog]"
+# end
